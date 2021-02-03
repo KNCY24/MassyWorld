26734bdb5e6091ffcf0f300796120498819b8133
KNCY24 <kency.mariema@etud.univ-jfc.fr>
2021-02-03 09:32:06 +0100

    Initial commit

---
+++ src/main/java/com/RSS/MRM/MassyWorld/MassyWorldApplication.java
@@ -0,0 +1,13 @@
+package com.RSS.MRM.MassyWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassyWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassyWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RSS/MRM/MassyWorld/MassyWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RSS.MRM.MassyWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassyWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
