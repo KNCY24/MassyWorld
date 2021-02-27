12c5c4a7c42f4c21e3945d7549f60b9002da5e47
erouss01 <emma.rousseau@etud.univ-jfc.fr>
2021-02-27 03:16:32 +0100

    test

--- src/main/java/com/RSS/MRM/MassyWorld/JerseyConfig.java
+++ src/main/java/com/RSS/MRM/MassyWorld/JerseyConfig.java
@@ -8,7 +8,7 @@ import javax.ws.rs.ApplicationPath;
 @Component
 @ApplicationPath("/adventureisis")
 public class JerseyConfig extends ResourceConfig {
-
+//
     public JerseyConfig() {
         register(Webservice.class);
         register(CORSResponseFilter.class);
